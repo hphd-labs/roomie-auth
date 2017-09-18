a0a8fa4dad5c4106918ed515d277fa623bdfa40d
Andrew Burian <andrew@axiomzen.co>
2017-09-18 09:54:22 -0700

    More robust content type decoding

--- helpers.go
+++ helpers.go
@@ -23,20 +23,48 @@ func RenderJSON(w http.ResponseWriter, obj interface{}) error {
 func AcceptsJSON(r *http.Request) bool {
 	// if there's content, make sure it's JSON
 	if r.ContentLength > 0 {
-		contentType := r.Header.Get("Content-Type")
-		if !strings.HasPrefix(contentType, "application/json") {
+		contentTypes := ParseContentTypes(r.Header.Get("Content-Type"))
+		if contentTypes[0].Type != "application/json" {
 			return false
 		}
 	}
 
 	// make sure JSON is in the accepts
-	for _, accpetEntry := range r.Header["Accept"] {
-		for _, entry := range strings.Split(accpetEntry, ",") {
-			if strings.HasPrefix(entry, "application/json") {
-				return true
-			}
+	for _, acceptType := range ParseContentTypes(r.Header.Get("Accept")) {
+		if acceptType.Type == "application/json" {
+			return true
 		}
 	}
 
 	return false
+}
+
+type ContentType struct {
+	Type    string
+	Options map[string]string
+}
+
+func ParseContentTypes(data string) []*ContentType {
+	types := make([]*ContentType, 0, 1)
+
+	for _, entry := range strings.Split(data, ",") {
+		t := &ContentType{
+			Options: make(map[string]string),
+		}
+
+		components := strings.Split(entry, ";")
+		t.Type = components[0]
+
+		for _, opt := range components[1:] {
+			values := strings.Split(opt, "=")
+			if len(values) != 2 {
+				continue
+			}
+			key := strings.TrimSpace(values[0])
+			t.Options[key] = strings.TrimSpace(values[1])
+		}
+
+		types = append(types, t)
+	}
+	return types
 }
