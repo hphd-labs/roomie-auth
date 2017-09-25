e602fcf848cae15298fad7b301df6e7de3e202ee
Andrew Burian <andrew@axiomzen.co>
2017-09-25 00:08:02 -0700

    Upgraded default cost

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Environment variables
 - `PORT` port to bind to (Default "http")
 - `DATABASE_URL` postgres connection string (Default to localhost)
+- `PASSWORD_COST` bcrypt cost value to use (Default 12)
--- config.go
+++ config.go
@@ -2,6 +2,6 @@ package main
 
 type Config struct {
 	Port         string `default:"http"`
-	PasswordCost int    `default:"10"`
-	DatabaseUrl  string `split_words:"true" default:"postgres://postgres:root@localhost:5432/postgres?sslmode=disable"`
+	PasswordCost int    `default:"12" split_words:"true"`
+	DatabaseUrl  string `default:"postgres://postgres:root@localhost:5432/postgres?sslmode=disable" split_words:"true"`
 }
